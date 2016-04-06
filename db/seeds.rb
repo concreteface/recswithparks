5ec5f1dea2cbf50b2cedcf1721ae4e9b93ae3285
evanlouden <evanlouden@gmail.com>
2016-04-06 17:28:18 -0400

    added upvote and downvote feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+FactoryGirl.create(:user)
+FactoryGirl.create(:user)
+Park.create([{ name: 'Boston Public Garden #1', street: 'Beacon', description: 'Super cool garden'}, {name: 'Boston Public Garden #2', street: 'Beacon', description: 'Super cool garden'}])
+Review.create([{ rating: 4, body: 'This park is really cool 1', user_id: 1, park_id: 1}])
+Review.create([{ rating: 3, body: 'This park is really cool 2', user_id: 1, park_id: 2}])
+Review.create([{ rating: 2, body: 'This park is really cool 3', user_id: 2, park_id: 1}])
+Review.create([{ rating: 1, body: 'This park is really cool 4', user_id: 2, park_id: 2}])
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,17 +3,30 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/poltergeist'
 require 'launchy'
 include Warden::Test::Helpers
 Warden.test_mode!
+
+Capybara.javascript_driver = :poltergeist
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
+end
+
+def expect_no_page_reload
+  page.evaluate_script "$(document.body).addClass('not-reloaded')"
+  yield
+  expect(page.has_css?("div")).to be true
+end
+
+def clear_users
+  User.delete_all
 end
