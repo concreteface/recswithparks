ad70c2e21aa9d61db8b996e0a47b2bcaf886249d
concreteface <concreteface@users.noreply.github.com>
2016-04-06 10:39:48 -0400

    add line after visit

--- spec/features/user_sees_list_of_parks_spec.rb
+++ spec/features/user_sees_list_of_parks_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 feature "user sees all the parks" do
   scenario "unauthenticated user visits index" do
-    first_park = FactoryGirl.create(:park) 
-    second_park = FactoryGirl.create(:park) 
-    third_park = FactoryGirl.create(:park) 
+    @first_park = FactoryGirl.create(:park) 
+    @second_park = FactoryGirl.create(:park) 
+    @third_park = FactoryGirl.create(:park) 
     visit "/"
-    expect(page).to have_content(first_park.name)
-    expect(page).to have_content(second_park.name)
-    expect(page).to have_content(third_park.name)
+
+    expect(page).to have_content(@first_park.name)
+    expect(page).to have_content(@second_park.name)
+    expect(page).to have_content(@third_park.name)
   end
 end
