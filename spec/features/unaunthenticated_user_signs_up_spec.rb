716319d892902cb85a8f942d4d7a040925677d10
Meredith Bubier Ross <meredithbubier@icloud.com>
2016-04-06 17:12:19 -0400

    started carrierwave, working on tests and uploading

--- spec/features/unaunthenticated_user_signs_up_spec.rb
+++ spec/features/unaunthenticated_user_signs_up_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
-#[]User creates account
-#[]User can sign in
-#[]User can sign out
+#[X]User creates account
+#[X]User can sign in
+#[X]User can sign out
+#[]User can add avatar
 #[]User can update information
 #[]User can delete account
 feature "unauthenticated user can create account" do
   scenario "visit new user page" do
     visit '/users/sign_up'
+
     expect(page).to have_content('Email')
     fill_in 'Email', with: 'yo@ho.com'
-    fill_in "Password", with: 'password'
-    fill_in "Password confirmation", with: 'password'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
     click_button 'Sign up'
+
     expect(page).to have_content('yo@ho.com')
     expect(page).to have_content('Parks')
     expect(page).not_to have_content('Log in')
