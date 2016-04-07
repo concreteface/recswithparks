5af1cb62c908c743e3f7d1c7bc0cf8f6b127dac3
evanlouden <evanlouden@gmail.com>
2016-04-07 14:47:03 -0400

    hound fixes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,6 @@ class ReviewsController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-
     @park = Park.find(params[:park_id])
     @review = Review.new(review_params)
     @review.park = @park
@@ -22,6 +21,5 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:rating, :body)
-
   end
 end
