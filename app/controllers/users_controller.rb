562c6dc95af74e1f5f20b56389fa13c755c1f7b0
concreteface <concreteface@users.noreply.github.com>
2016-04-11 14:08:55 -0400

    working on admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
+
+  before_filter :authorize, only: :index
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews
+  end
+
+  private
+
+  def authorize
+    unless current_user.try(:admin?)
+      flash[:error] = 'You lack the necessary permission to view this page'
+      redirect_to root_path
+    end
   end
 end
