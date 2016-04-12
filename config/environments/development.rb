2ac82cd0ff2c04781cd8aeed0daf064d7dfcbafa
concreteface <concreteface@users.noreply.github.com>
2016-04-12 13:40:30 -0400

    mid email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,19 +14,29 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "localhost",
-    port: 1025
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "localhost",
+  #   port: 1025
+  # }
 
   config.action_mailer.default_url_options = {
     host: "localhost",
     port: 3000
   }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mailgun.org',
+    port:                 587,
+    domain:               'recswithparks.heroku.com',
+    user_name:            ENV['MAILGUN_USERNAME'],
+    password:             ENV['MAILGUN_PASSWORD'],
+    authentication:       'plain',
+  enable_starttls_auto: true  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
