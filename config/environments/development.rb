85b82d26818b0a27a42e055bba327c80a3f87f7b
John M <concreteface@users.noreply.github.com>
2016-04-13 09:35:03 -0400

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
     host: "localhost",
     port: 3000
   }
-  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
