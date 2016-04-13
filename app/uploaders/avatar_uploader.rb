ada5f0e5939631b61f37819656830c7669da13ae
evanlouden <evanlouden@gmail.com>
2016-04-13 14:50:31 -0400

    whole lotta work

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,13 +20,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  #
-  # end
+  def default_url
+    "http://i1252.photobucket.com/albums/hh561/evanlouden/no-pic-uploaded.png"
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -42,13 +38,22 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+#   unless Rails.env.test?
+#     def filename
+#       "#{secure_token}.#{file.extension}" if original_filename.present?
+#     end
+#   end
+# ​
+#   protected
+#
+#   def secure_token
+#     var = :"@#{mounted_as}_secure_token"
+#     model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+#   end
 end
