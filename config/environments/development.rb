2214aaa4d74761d242a6e1883e3c5f3479ca2993
concreteface <concreteface@users.noreply.github.com>
2016-04-13 13:41:31 -0400

    fixed dt comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,10 +21,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  # config.action_mailer.smtp_settings = {
-  #   address: "localhost",
-  #   port: 1025
-  # }
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 1025
+  }
 
   config.action_mailer.default_url_options = {
     host: "localhost",
@@ -32,14 +32,6 @@ Rails.application.configure do
   }
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_NAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
