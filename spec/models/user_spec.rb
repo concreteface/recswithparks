98cfc90fd4c998d1c64724e5609b5cf14670f91a
John M <concreteface@users.noreply.github.com>
2016-04-13 15:35:43 -0400

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '#add_avatar' do
   	clear_users
-    let(:user){ User.new(email: 'yo@ho.com', password: 'password')}
+    let(:user) { User.new(email: 'yo@ho.com', password: 'password') }
     it 'takes a file location as an argument' do
       user.add_avatar('goats_avatar.jpg')
       expect(user.avatar_url).to eq("/uploads/user/avatar/#{user.id}/goats_avatar.jpg")
