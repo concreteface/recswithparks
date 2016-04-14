3f0552c7937d47f35f0f10c2e0abfafa8e0dff28
Meredith Bubier Ross <meredithbubier@icloud.com>
2016-04-14 14:40:58 -0400

    menu button added

--- spec/features/user_sees_profile_page_spec.rb
+++ spec/features/user_sees_profile_page_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
-feature "user sees profile page" do
+feature 'user sees profile page' do
   before (:each) do
     @user_1 = FactoryGirl.create(:user)
     @review_1 = FactoryGirl.create(:review, user: @user_1)
     @review_2 = FactoryGirl.create(:review, user: @user_1)
   end
 
-  scenario "user visits index page" do
+  scenario 'user visits index page' do
     login_as(@user_1)
-    visit "/"
-
-    click_link(@user_1.email)
+    visit '/'
+    within('div.top-bar-right') do
+      click_link(@user_1.email)
+    end
     expect(page).to have_css("img[src*='#{@user_1.avatar.url}']")
     expect(page).to have_content(@user_1.reviews.first.body)
     expect(page).to have_content(@user_1.reviews.second.body)
